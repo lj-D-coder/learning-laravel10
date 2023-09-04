052b188ed80e6c6735212d21c986fcc74aa56008
lj-D-coder <lj_1990@live.com>
2023-09-04 19:55:20 +0530

    posts routing using html files

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="/app.css">
+<title>my Blog</title>
+
+<body>
+    <article>
+        <?= $post;?>
+    </article>
+    <a href="/">Go Back</a>
+
+</body>
