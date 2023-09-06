38d069e7d4f451a462e54f26db3c9c05a419cc56
lj-D-coder <lj_1990@live.com>
2023-09-06 22:50:41 +0530

    cooction and arrow function

--- routes/web.php
+++ routes/web.php
@@ -18,32 +18,17 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-
-    $files = File::files(resource_path("posts"));
     //using laravel collection
-    $posts = collect($files)->map(function($file){
-        $document= YamlFrontMatter::parseFile($file);
-           return new Post(
+    $posts = collect(File::files(resource_path("posts")))
+    ->map(fn($file)=>YamlFrontMatter::parseFile($file))
+    ->map(fn ($document)=>new Post(
             $document->title,
             $document->excerpt,
             $document->date,
             $document->body(),
             $document->slug
-           );
+           ));
 
-    });
-
-    // //using array_map instead of foreach loop
-    // $posts = array_map(function ($file){
-    //     $document= YamlFrontMatter::parseFile($file);
-    //    return new Post(
-    //     $document->title,
-    //     $document->excerpt,
-    //     $document->date,
-    //     $document->body(),
-    //     $document->slug
-    //    );
-    // }, $files);
     return view('posts',[
         'posts' => $posts
     ]);
